122de15f97b81742ef5e4cba0b8ef83f7eb5e7bf
Ankur Shrivastava <ankur@thecarousell.com>
2016-10-18 11:30:40 +0800

    updated theme

--- tmux-powerline-themes/ankurs.sh
+++ tmux-powerline-themes/ankurs.sh
@@ -24,10 +24,12 @@ TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SE
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
     TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
         "tmux_session_info 148 234" \
+        "weather 37 255" \
+        "load 237 167" \
         #"hostname 33 0" \
         #"ifstat 30 255" \
-        "ifstat_sys 30 255" \
-        "lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
+        #"ifstat_sys 30 255" \
+        #"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
         #"wan_ip 24 255" \
         #"vcs_branch 29 88" \
         #"vcs_compare 60 255" \
@@ -45,10 +47,10 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
         #"now_playing 234 37" \
         #"uptime 234 37" \
         #"cpu 240 136" \
-        "load 237 167" \
+        #"load 237 167" \
         #"tmux_mem_cpu_load 234 136" \
         "battery 137 127" \
-        "weather 37 255" \
+        #"weather 37 255" \
         #"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
         #"xkb_layout 125 117" \
         "date_day 235 136" \
