0bde0e0a3a56ccfb01aaba1fbcc2e176629c1778
Ankur Shrivastava <ankur@ankurs.com>
2018-10-08 17:35:17 +0800

    updating multiple componenets

--- tmux-powerline-themes/ankurs.sh
+++ tmux-powerline-themes/ankurs.sh
@@ -18,7 +18,8 @@ TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLO
 TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR:-$TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD}
 TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR:-$TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}
 export TMUX_POWERLINE_SEG_NOW_PLAYING_MUSIC_PLAYER="spotify"
-export TMUX_POWERLINE_SEG_NOW_PLAYING_MAX_LEN="30"
+export TMUX_POWERLINE_SEG_NOW_PLAYING_MAX_LEN="25"
+#export TMUX_POWERLINE_SEG_WEATHER_LOCATION="1062617"
 # Format: segment_name background_color foreground_color [non_default_separator]
 
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
@@ -44,7 +45,7 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
         #"pwd 89 211" \
         #"mailcount 9 255" \
         "now_playing 237 37" \
-        "uptime 234 37" \
+        #"uptime 234 37" \
         #"cpu 240 136" \
         #"load 237 167" \
         #"tmux_mem_cpu_load 234 136" \
@@ -52,7 +53,7 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
         #"weather 37 255" \
         #"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
         #"xkb_layout 125 117" \
-        #"weather 37 255" \
+        "weather 37 255" \
         "date_day 235 136" \
         "date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
         "time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
---
+++ tmuxifier-layouts/code.window.sh
@@ -0,0 +1,13 @@
+window_root "~/code/go/src/github.com/carousell/$SVC"
+
+new_window "code"
+
+split_h 20
+select_pane 1
+split_v 20
+split_h 50
+
+run_cmd "dock" 1
+run_cmd "dock" 2
+run_cmd "dock" 3
+run_cmd "dock" 4
