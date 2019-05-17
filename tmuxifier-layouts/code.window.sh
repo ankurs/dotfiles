ac3b2579af019afea16296fc34615b6061dcffaa
Ankur Shrivastava <ankur@ankurs.com>
2019-05-17 13:34:52 +0800

    updated vimwiki

--- tmuxifier-layouts/code.window.sh
+++ tmuxifier-layouts/code.window.sh
@@ -7,7 +7,8 @@ select_pane 1
 split_v 20
 split_h 50
 
-run_cmd "dock; enl_venv" 1
-run_cmd "dock; enl_venv" 2
-run_cmd "dock; enl_venv" 3
-run_cmd "dock; enl_venv" 4
+run_cmd "enl_venv" 1
+run_cmd "enl_venv" 2
+run_cmd "enl_venv" 3
+run_cmd "enl_venv" 4
+select_pane 1
