83ceaffba967b8025f6c1803e8460eb341b1fb22
Ankur Shrivastava <ankur@ankurs.com>
2020-03-29 14:31:58 +0800

    updating setup to work with Fedora

--- setup.sh
+++ setup.sh
@@ -1,28 +1,93 @@
-#!/bin/bash
+#!/bin/bash -e
+
+UPDATE=""
+
+if [[ ! -z "$1" ]]
+then
+    UPDATE="yes"
+fi
+
+function setup_mac() {
+    echo "setting up mac"
+    brew cask install java
+    cat ./brew_tap | xargs -L 1 brew tap
+    cat ./brew_list | xargs -L 1 brew install %
+    cat ./brew_cask_list | xargs -L 1 brew cask install
+}
+
+function setup_fedora() {
+    if [[ -z $UPDATE ]]
+    then
+        echo "Setting up Fedora"
+        sudo systemctl enable sshd
+        sudo systemctl start sshd
+        echo "setting up RPM Fusion"
+        sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+        sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    fi
+    sudo dnf update-minimal -y
+    echo "setting up Development Tools"
+    sudo dnf groupinstall "Development Tools" -y
+    sudo dnf install cmake make python-devel zsh gcc-c++ -y
+    echo "installing snap"
+    sudo dnf install -y snapd
+    if [[ -z $UPDATE ]]
+    then
+        sudo ln -s /var/lib/snapd/snap /snap
+    fi
+
+    cat ./snap_list | xargs -L 1 sudo snap install
+    cat ./dnf_list | xargs -L 1 sudo dnf install -y
+}
+
+function do_setup() {
+    if [[ $(uname) == "Darwin" ]]
+    then
+        setup_mac
+    fi
+
+    if [[ $(uname) == "Linux" ]]
+    then
+        if [[ -f /etc/os-release ]]
+        then
+            source /etc/os-release
+            if [[ $NAME == "Fedora" ]]
+            then
+                setup_fedora
+            fi
+        fi
+    fi
+}
+
 git submodule init
 git submodule update
-bash fonts/install.sh
-PWD=`pwd`
-ln -s $PWD/dot-profile ~/.profile
-ln -s $PWD/dot-tmux.conf ~/.tmux.conf
-ln -s $PWD/dot-vimrc ~/.vimrc
-ln -s $PWD/dot-zshrc ~/.zshrc
-ln -s $PWD/dot-tmux-powerlinerc ~/.tmux-powerlinerc
-ln -s $PWD/dot-mostrc ~/.mostrc
-ln -s $PWD/dotgitconfig ~/.gitconfig
-ln -s $PWD/dot-gitignore ~/.gitignore
-mkdir -p ~/.config/nvim/
-mkdir -p ~/.vim/bundle
-ln -s ~/.vimrc ~/.config/nvim/init.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+
+if [[ -z $UPDATE ]]
+then
+    bash fonts/install.sh
+    PWD=`pwd`
+    ln -s $PWD/dot-profile ~/.profile
+    ln -s $PWD/dot-tmux.conf ~/.tmux.conf
+    ln -s $PWD/dot-vimrc ~/.vimrc
+    ln -s $PWD/dot-zshrc ~/.zshrc
+    ln -s $PWD/dot-tmux-powerlinerc ~/.tmux-powerlinerc
+    ln -s $PWD/dot-mostrc ~/.mostrc
+    ln -s $PWD/dotgitconfig ~/.gitconfig
+    ln -s $PWD/dot-gitignore ~/.gitignore
+    mkdir -p ~/.config/nvim/
+    mkdir -p ~/.vim/bundle
+    ln -s ~/.vimrc ~/.config/nvim/init.vim
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
+else
+    echo "Update requested skipping initial setup"
+fi
+
+do_setup
+
 python ~/.vim/bundle/YouCompleteMe/install.py  --rust-completer --clang-completer
-brew cask install java
-cat ./brew_tap | xargs -n 1 brew tap
-cat ./brew_list | xargs -n 1 brew install
-cat ./brew_cask_list | xargs -n 1 brew cask install
 vim +GoUpdateBinaries +qall
-pip install neovim
-pip install bashate
+pip install neovim --user
+pip install bashate --user
 ## install java 1.8
-jabba install zulu@1.8
+#jabba install zulu@1.8
