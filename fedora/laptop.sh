516cff0809abe93a3c227485f36ad3e2dba05dc0
Ankur Shrivastava <ankur@ankurs.com>
2020-04-12 22:27:12 +0800

    updating laptop

--- fedora/laptop.sh
+++ fedora/laptop.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -e
+echo "Installing packages"
+sudo dnf install tlp tlp-rdw powertop lm_sensors -y
 echo "Setting up tlp"
-sudo dnf install tlp tlp-rdw powertop -y
 sudo systemctl enable tlp
 sudo systemctl start tlp
 sudo tlp start
+
+echo "setting up sensors"
+sudo sensors-detect
+
+echo "setting up xsensors"
+sudo dnf install xsensors -y
