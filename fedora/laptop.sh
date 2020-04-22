a6cd38d1585cc4aed7e93f3d19337f1c57fe829e
Ankur Shrivastava <ankur@ankurs.com>
2020-04-22 14:28:48 +0800

    enable mbpfan

--- fedora/laptop.sh
+++ fedora/laptop.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -e
 echo "Installing packages"
-sudo dnf install tlp tlp-rdw powertop lm_sensors -y
+sudo dnf install tlp tlp-rdw powertop lm_sensors mbpfan -y
 echo "Setting up tlp"
 sudo systemctl enable tlp
 sudo systemctl start tlp
 sudo tlp start
+
+systemctl enable mbpfan
+systemctl start mbpfan
 
 echo "setting up sensors"
 sudo sensors-detect
