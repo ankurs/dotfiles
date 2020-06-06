5cedbaec8fd2f7e9fc66ae5de2534323fc26f0b5
Ankur Shrivastava <ankur@ankurs.com>
2020-06-06 11:39:43 +0800

    updating multi monitor setup

--- fedora/virt.sh
+++ fedora/virt.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 sudo dnf group install --with-optional virtualization -y
+sudo usermod --append --groups libvirt $USER
 sudo systemctl start libvirtd
 sudo systemctl enable libvirtd
 
