db5a107b1fcfc9a419c6070946725eab89c39f5d
Ankur Shrivastava <ankur@ankurs.com>
2020-06-15 16:34:59 +0800

    updating vimrc

--- setup_lang_server.sh
+++ setup_lang_server.sh
@@ -5,3 +5,5 @@ npm i -g typescript-language-server bash-language-server
 pip install vim-vint --user --upgrade
 go get -u golang.org/x/tools/...
 go get -u github.com/stamblerre/gocode
+
+curl -fLo ~/.java-lang-server/jdt-language-server-latest.tar.gz --create-dir http://download.eclipse.org/jdtls/snapshots/jdt-language-server-latest.tar.gz
