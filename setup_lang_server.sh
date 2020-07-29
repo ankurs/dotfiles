e4db9aaee4470b180b10a7bdfd4e3fb0bf7d8701
Ankur Shrivastava <ankur@ankurs.com>
2020-07-29 23:58:04 +0800

    updating for rust

--- setup_lang_server.sh
+++ setup_lang_server.sh
@@ -7,3 +7,7 @@ go get -u golang.org/x/tools/...
 go get -u github.com/stamblerre/gocode
 
 curl -fLo ~/.java-lang-server/jdt-language-server-latest.tar.gz --create-dir http://download.eclipse.org/jdtls/snapshots/jdt-language-server-latest.tar.gz
+
+rustup toolchain add nightly
+cargo +nightly install racer
+rustup default nightly
