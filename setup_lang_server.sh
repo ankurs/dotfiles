0295b80a37e8724afb70457e0cff625dc9a107a7
Ankur Shrivastava <ankur@ankurs.com>
2020-09-02 11:26:04 +0800

    updating lang server setup

--- setup_lang_server.sh
+++ setup_lang_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pip install pynvim bashate --user --upgrade
 pip install python-language-server[all] --user --upgrade
-npm i -g typescript-language-server bash-language-server
+npm i -g typescript-language-server bash-language-server --upgrade
 pip install vim-vint --user --upgrade
 go get -u golang.org/x/tools/...
 go get -u github.com/stamblerre/gocode
